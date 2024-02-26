97a2c4c71816fcbd5626777378e76042f59ae2af
preeti <karnwalpreeti102@gmail.com>
2024-02-26 15:49:18 +0530

    modified controller

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -1,23 +1,28 @@
 const catchAsync = require("../utils/catchAsync");
 const appError = require("../utils/appError");
 const dotenv = require("dotenv");
-dotenv.config("");
+dotenv.config("./config.env");
 
+const sdk = require("api")("@nodereal/v1.5#10tuepy34l8ithl0p");
 
-const sdk = require('api')('@nodereal/v1.5#10tuepy34l8ithl0p');
+exports.latestBlock = catchAsync(async (req, res) => {
+  //fetching block number
+  const data = await sdk.ethBlocknumber(
+    {
+      id: 1,
+      jsonrpc: "2.0",
+      method: "eth_blockNumber",
+    },
+    {
+      apiKey: process.env.api_key,
+    }
+  );
 
-exports.latestBlock = catchAsync(async (req,res) =>{
+  console.log(data);
 
-    //fetching block number
-    const data = await sdk.ethBlocknumber({
-        id: 1,
-        jsonrpc: '2.0',
-        method: 'eth_blockNumber'
-      }, {
-        apiKey: process.env.api_key
-      })
-    res.status(200).json({
-        status: 'success',
-        data
-    })
-})
+  //send data
+  res.status(200).json({
+    status: "success",
+    data,
+  });
+});
--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const routes = require("./");
+const routes = require("./routes/routes");
 
 const app = express();
-
-
 
 dotenv.config("");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-// app.use("/api/ethereum", routes);
+app.use("/api", routes);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
+console.log(port);
 app.listen(port, () => {
   console.log(`listening to the port ${port}`);
 });
