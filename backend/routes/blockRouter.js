5cd8ae04a6ffc87d540fa964a73f8fb6ff7c6788
preeti <karnwalpreeti102@gmail.com>
2024-02-28 17:08:22 +0530

    fixed routing

--- backend/routes/blockRouter.js
+++ backend/routes/blockRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const blockController = require("./../controllers/blockController");
 
 router.get("/latest", blockController.latestBlock);
-router.get("/:num", blockController.latestBlockByNumber);
 router.get("/all", blockController.getAllBlocks);
+router.get("/:num", blockController.latestBlockByNumber);
 
 module.exports = router;
--- backend/routes/txRouter.js
+++ backend/routes/txRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const txController = require("./../controllers/txController");
 
 // router.get("/latest", txController.latestTx);
-router.get("/:txhash", txController.getTxByHash);
 router.get("/all", txController.getAllTxs);
+router.get("/:txhash", txController.getTxByHash);
 
 module.exports = router;
